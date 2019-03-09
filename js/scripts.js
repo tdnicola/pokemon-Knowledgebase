b7c28e58b83aff114076152ce9d12e1a3706bf1b
tdnicola <tdnicola@gmail.com>
2019-03-09 15:34:44 -0700

    added loadList to retrieve pokemon names. added LoadDetails to retrieve pokemon details. added addListItem function to add new li-button with pokemon name

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,50 @@ var pokemonRepository = (function () {
     return repository;
   }
 
+  function addListItem (pokemon) {
+    var $ul = $('ul')
+    var $li = $('<li class="pokemonName">test</li>')
+    var $button = $('<button type="button">' + pokemon.name + '</button>')
+    $ul.append($li)
+    $li.append($button)
+
+    // add modal to button. Not set up yet
+    // $button.on('click'. function (event) {
+    //   addmodal
+    // })
+  }
+
+  function loadList () {
+    return $.ajax(apiUrl).then(function (response) {
+      response.results.forEach(function (item) {
+        var pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        }
+        add(pokemon)
+      })
+    }).catch(function (e) {
+      console.error(e)
+    })
+  }
+
+  function loadDetails (item) {
+    return $.ajax(item.detailsUrl).then(function (response) {
+      response.results.forEach(function (details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types.map(function (item) {return item.type.name})
+      })
+    })
+  }
+
+
+  return {
+    loadList: loadList,
+    loadDetails: loadDetails,
+    addListItem: addListItem,
+    getAll: getAll
+  }
+
+
 })
